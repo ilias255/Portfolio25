2478352ffca19af455de10ca9d289d3fef072734
ilias25 <ilias.lafrej@epitech.eu>
2025-01-17 16:43:19 +0100

    portfolio

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function typeEffect() {
 document.addEventListener("DOMContentLoaded", typeEffect);
 
 
-// Effet sur mon blase
+// Effet bienvenue + blase
 const glitch = document.getElementById("glitch");
 
 glitch.innerHTML = glitch.textContent
@@ -31,3 +31,40 @@ document.addEventListener("mousemove", (e) => {
 });
 
 
+
+//effet particles js
+document.addEventListener("DOMContentLoaded", () => {
+    if (document.getElementById("particles-js")) {
+        particlesJS("particles-js", {
+            particles: {
+                number: { value: 140, density: { enable: true, value_area: 700 } },
+                color: { value: "#ffffff" },
+                shape: { type: "circle" },
+                opacity: { value: 0.5 },
+                size: { value: 3, random: true },
+                line_linked: { enable: true, distance: 150, color: "#ffffff", opacity: 0.4, width: 1 },
+                move: {
+                    enable: true,
+                    speed: 2,
+                    direction: "none",
+                    random: false,
+                    straight: false,
+                    out_mode: "out",
+                    attract: { enable: true, rotateX: 600, rotateY: 1200 }
+                }
+            },
+            interactivity: {
+                detect_on: "canvas",
+                events: {
+                    onhover: { enable: true, mode: "repulse" },
+                    onclick: { enable: true, mode: "push" }
+                },
+                modes: {
+                    repulse: { distance: 100, duration: 0.4 },
+                    push: { particles_nb: 4 }
+                }
+            },
+            retina_detect: true
+        });
+    }
+});
